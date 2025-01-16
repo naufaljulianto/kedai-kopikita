df0e0719639b7b487f7e1aa22bf96aea83830776
Muh. Naufal Julianto <novalja1235@gmail.com>
2025-01-16 20:23:31 +0700

    Menambahkan masking CSS dan fitur keranjang belanja

--- js/script.js
+++ js/script.js
@@ -1,15 +1,64 @@
-// Toggle class active
+// Toggle class active hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// Klik di luar sidebar untuk menghilangkan menu
+// Toggle class active search bar
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.getElementById("search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Toggle class active shopping cart
+const shoppingCart = document.querySelector(".shopping-cart");
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
+
+// Klik di luar elemen
 const hamburger = document.querySelector("#hamburger-menu");
+const btnSearch = document.getElementById("search-button");
+const btnShoppingCart = document.getElementById("shopping-cart-button");
 
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
+
+  if (!btnSearch.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+
+  if (!btnShoppingCart.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
 });
+
+// Modal Box
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButton = document.querySelector(".item-detail-button");
+
+itemDetailButton.onclick = (e) => {
+  itemDetailModal.style.display = "flex";
+  e.preventDefault();
+};
+
+// klik tombol close modal
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+
+// klik di luar modal
+window.onclick = (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+};
